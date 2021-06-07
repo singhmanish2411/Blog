5a74f343609c56d748a11b517e4656ea42e19ac6
Manish <manishkr0360@gmail.com>
2021-06-08 01:34:47 +0530

    added forms

--- blog/tests.py
+++ blog/tests.py
@@ -41,6 +41,20 @@ class BlogTests(TestCase):
     def test_post_detail_view(self):
         response = self.client.get('/post/1')
         no_response = self.client.get('/post/100000')
+        self.assertEqual(response.status_code,200)
         self.assertEqual(no_response.status_code,404)
         self.assertContains(response,'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+    
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'),
+        {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
